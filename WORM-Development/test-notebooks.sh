e80eed6fb7a0cf0fbccef28e604295df753108a9
gmboyer <gmboyer@asu.edu>
2021-05-21 15:49:43 -0700

    Fixes to tests in test-notebooks.sh

--- WORM-Development/test-notebooks.sh
+++ WORM-Development/test-notebooks.sh
@@ -6,7 +6,7 @@ conda run -n python3 pytest --nbval 2-Reaction-Properties/1-Reaction-Properties.
 conda run -n python3 pytest --nbval 2-Reaction-Properties/2-Activity-Diagrams.ipynb
 conda run -n python3 pytest --nbval 3-Aqueous-Speciation/1-Introduction-to-Aq-Speciation/1-Intro-to-Aq-Speciation.ipynb
 conda run -n python3 pytest --nbval 3-Aqueous-Speciation/1-Introduction-to-Aq-Speciation/2-Intro-to-Multi-Aq-Speciation.ipynb
-conda run -n python3 pytest --nbval 3-Aqueous-Speciation/1-Introduction-to-Aq-Speciation/2-Multi-Sample-Speciation/Multi-Sample-Speciation.ipynb
-conda run -n python3 pytest --nbval 3-Aqueous-Speciation/1-Introduction-to-Aq-Speciation/3-Charge-Balancing/Charge-Balancing.ipynb
+conda run -n python3 pytest --nbval 3-Aqueous-Speciation/2-Multi-Sample-Speciation/Multi-Sample-Speciation.ipynb
+conda run -n python3 pytest --nbval 3-Aqueous-Speciation/3-Charge-Balancing/Charge-Balancing.ipynb
 conda run -n python3 pytest --nbval 4-Custom-Database/Custom-Database.ipynb
 conda run -n python3 pytest --nbval 5-Extras/speciate-3i-file/speciate-3i-file.ipynb
