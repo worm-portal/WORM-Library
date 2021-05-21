c1413024874d7e71bff8c2ae3ec9bf84cfe19b3e
gmboyer <gmboyer@asu.edu>
2021-05-21 15:21:48 -0700

    Removed chmod line in shell script for pytest testing of notebooks.

--- WORM-Development/test-notebooks.sh
+++ WORM-Development/test-notebooks.sh
@@ -4,4 +4,3 @@ conda run -n python3 pytest --nbval 1-Introduction/1-Introduction.ipynb
 conda run -n python3 pytest --nbval 1-Introduction/2-Notebook-Intro.ipynb
 conda run -n python3 pytest --nbval 2-Reaction-Properties/1-Reaction-Properties.ipynb
 conda run -n python3 pytest --nbval 2-Reaction-Properties/2-Activity-Diagrams.ipynb
-sudo chmod 600 /home/shock/repos/pytest-worm-library/worm-library
