44a9d6e99cb026334e0e0f499b81b9e034379d3e
gmboyer <gmboyer@asu.edu>
2021-07-02 15:23:29 -0700

    updated to 1.1.0-beta. Added introductory notebook and exercise for custom database creation. Added a demo notebook for alter/suppress options.

--- WORM-Development/CHANGELOG.md
+++ WORM-Development/CHANGELOG.md
@@ -9,6 +9,13 @@ and adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
 - No unreleased changes yet
 
+## [1.1.0-beta] - 2021-7-2
+
+### Added
+
+- Aqueous Speciation: Introductory and exercise notebooks for custom database creation 
+- Aqueous Speciation: Alter/Suppress option demo notebook
+
 ## [1.0.0-beta] - 2021-5-24
 
 ### Added
@@ -20,5 +27,6 @@ and adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 - split pyCHNOSZ notebook into two: one for calculating reaction properties and another for creating diagrams
 - pyCHNOSZ example notebooks no longer display hexidecimal ids that interfere with Pytest tests
 
-[Unreleased]: https://gitlab.com/worm1/worm-library/-/compare/v1.0.0-beta...master
+[Unreleased]: https://gitlab.com/worm1/worm-library/-/compare/v1.1.0-beta...master
+[1.1.0-beta]: https://gitlab.com/worm1/worm-library/-/compare/v1.0.0...v1.1.0-beta
 [1.0.0-beta]: https://gitlab.com/worm1/worm-library/-/compare/v0.9.0...v1.0.0-beta
--- WORM-Development/test-notebooks.sh
+++ WORM-Development/test-notebooks.sh
@@ -7,9 +7,12 @@ pytest --nbval 3-Aqueous-Speciation/1-Introduction-to-Aq-Speciation/1-Intro-to-A
 pytest --nbval 3-Aqueous-Speciation/1-Introduction-to-Aq-Speciation/2-Intro-to-Multi-Aq-Speciation.ipynb
 pytest --nbval 3-Aqueous-Speciation/2-Multi-Sample-Speciation/Multi-Sample-Speciation.ipynb
 pytest --nbval 3-Aqueous-Speciation/3-Charge-Balancing/Charge-Balancing.ipynb
-pytest --nbval 3-Aqueous-Speciation/4-Custom-Database/Custom-Database.ipynb
-pytest --nbval 3-Aqueous-Speciation/5-Extras/speciate-3i-file/speciate-3i-file.ipynb
-pytest --nbval 3-Aqueous-Speciation/5-Extras/speciate-6i-file/speciate-6i-file.ipynb
+pytest --nbval 3-Aqueous-Speciation/4-Custom-Database/1-Introduction/1-Introduction.ipynb
+pytest --nbval 3-Aqueous-Speciation/4-Custom-Database/1-Introduction/2-Exercise.ipynb
+pytest --nbval 3-Aqueous-Speciation/4-Custom-Database/Minimal-Example/Custom-Database.ipynb
+pytest --nbval 3-Aqueous-Speciation/5-Alter-and-Suppress/Alter-and-Suppress.ipynb
+pytest --nbval 3-Aqueous-Speciation/6-Extras/speciate-3i-file/speciate-3i-file.ipynb
+pytest --nbval 3-Aqueous-Speciation/6-Extras/speciate-6i-file/speciate-6i-file.ipynb
 pytest --nbval 4-Thermodynamic-Property-Estimation/Introduction-Aq-Organic-Estimator.ipynb
 pytest --nbval 4-Thermodynamic-Property-Estimation/Aq-Organics-Feature-Demo/Aqueous-Organic-Molecules.ipynb
 pytest --nbval WORKSHOP/WorkshopDemo.ipynb
