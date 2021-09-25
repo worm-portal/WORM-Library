263f2eb54b7b02836af1cd1ee800f9f8a49464f7
gmboyer <gmboyer@asu.edu>
2021-09-25 10:55:18 -0700

    Updated all interactive plots with chemical name super/subscripts

--- WORM-Development/.ipynb_checkpoints/test-notebooks-checkpoint.sh
+++ WORM-Development/.ipynb_checkpoints/test-notebooks-checkpoint.sh
@@ -1,8 +1,12 @@
 cd ..
 pytest --nbval 1-Introduction/1-Introduction.ipynb
 pytest --nbval 1-Introduction/2-Notebook-Intro.ipynb
+pytest --nbval "1-Introduction/Exploration-In-Geochemistry-(video).ipynb"
 pytest --nbval 2-Reaction-Properties/1-Reaction-Properties.ipynb
 pytest --nbval 2-Reaction-Properties/2-Activity-Diagrams.ipynb
+pytest --nbval 2-Reaction-Properties/3-Animated-Diagrams.ipynb
+pytest --nbval 2-Reaction-Properties/4-Univariant-Curves/1-Geothermometry.ipynb
+pytest --nbval 2-Reaction-Properties/4-Univariant-Curves/2-Univariate-Curves-T-P.ipynb
 pytest --nbval 3-Aqueous-Speciation/1-Introduction-to-Aq-Speciation/1-Intro-to-Aq-Speciation.ipynb
 pytest --nbval 3-Aqueous-Speciation/1-Introduction-to-Aq-Speciation/2-Intro-to-Multi-Aq-Speciation.ipynb
 pytest --nbval 3-Aqueous-Speciation/2-Multi-Sample-Speciation/Multi-Sample-Speciation.ipynb
